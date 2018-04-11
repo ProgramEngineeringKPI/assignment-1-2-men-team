dbf1048e48d65b19bdccee8129306f91533161a6
Vasniktel <vasniktel@gmail.com>
2018-04-11 11:02:45 +0300

    Reimplemented exception handling

--- d/main.d
+++ d/main.d
@@ -1,11 +1,11 @@
 import std.algorithm.iteration : filter, fold, map;
 import std.array : array, join, split;
-import std.conv : to;
+import std.conv : to, ConvException;
 import std.file : readText, write;
 import std.path : sep = dirSeparator;
 import std.stdio : stderr;
 
-enum Points : int {
+enum Points : uint {
   win = 3,
   draw = 1,
   lose = 0
@@ -27,41 +27,35 @@ string[][] parseCSV(string data, string delim = ",") {
   return data.split('\n').map!(line => line.split(delim)).filter!(el => !!el).array;
 }
 
-int points(int[] score) {
+uint points(uint[] score) {
   if (score[0] > score[1]) return Points.win;
   if (score[0] == score[1]) return Points.draw;
   return Points.lose;
 }
 
-bool tryValidate(string pair, out int[] result) {
+bool tryValidate(string pair, out uint[] result) {
   try {
-    string[] paired = pair.split(':');
-    if (paired.length == 0 || paired[0].length == 0 || paired[1].length == 0)
-      return false;
-
-    int[] nums = paired.map!(to!int).array;
-    if (nums[0] < 0 || nums[1] < 0) return false;
-
-    result = nums;
-  } catch (Throwable) {
+    result = pair.split(':').map!(to!uint).array;
+    if (result.length != 2) return false;
+  } catch (ConvException) {
     return false;
   }
   return true;
 }
 
-int[string] processData(string[][] data) {
+uint[string] processData(string[][] data) {
   return data.fold!((res, line) {
     res[line[0]] = line[1 .. $].map!((pair) {
-      int[] nums;
+      uint[] nums;
       if (!tryValidate(pair, nums))
         throw new Exception("Invalid input: <" ~ pair ~ "> in 'validate' function");
       return nums;
     }).fold!((acc, score) => acc + points(score))(0);
     return res;
-  })( (int[string]).init );
+  })( (uint[string]).init );
 }
 
-void printResult(int[string] data, string outfile, string delim = ",") {
+void printResult(uint[string] data, string outfile, string delim = ",") {
   string result = data.keys.map!(team => [team, data[team].to!string].join(delim)).join('\n');
   outfile.write(result);
 }
